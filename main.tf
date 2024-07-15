060c5a8941dd523087964dd679b37b4a4d0174a5
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2024-07-15 19:19:34 -0400

    module updated

--- main.tf
+++ main.tf
@@ -40,8 +40,8 @@ data "aws_lambda_function" "lambda" {
 
 resource "aws_api_gateway_method_response" "response_200" {
   rest_api_id = aws_api_gateway_rest_api.this.id
-  resource_id = aws_api_gateway_resource.this.id
-  http_method = aws_api_gateway_method.this.http_methods
+  resource_id = aws_api_gateway_resource.this[each.key].id
+  http_method = aws_api_gateway_method.this[each.key].http_methods
   status_code = "200"
   response_models = {
     "application/json" = "Empty"
