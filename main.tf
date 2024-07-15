f4a6ad8810d09d1466a013a242ff7af4ffb11af7
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2024-07-15 19:29:09 -0400

    module updated

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ resource "aws_api_gateway_method_response" "response_200" {
 }
 
 resource "aws_api_gateway_integration_response" "this" {
+  for_each = var.method_responses
   rest_api_id = aws_api_gateway_rest_api.this.id
   resource_id = aws_api_gateway_resource.this[each.key].id
   http_method = aws_api_gateway_method.this[each.key].http_methods
