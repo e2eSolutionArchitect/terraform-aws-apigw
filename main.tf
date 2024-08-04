df0af8a4e9a1e22a592a77ac75ca051d0e5d4e4d
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2024-08-03 20:41:27 -0400

    updated

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_api_gateway_rest_api" "this" {
-  name = var.apigw_name
-  tags = var.tags
+  name               = var.apigw_name
+  binary_media_types = ["application/json"]
+  tags               = var.tags
   endpoint_configuration {
     types = ["REGIONAL"]
   }
@@ -30,7 +31,62 @@ resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   type                    = each.value.integration_type
   uri                     = data.aws_lambda_function.lambda[each.value.resource_path].invoke_arn
-  integration_http_method = aws_api_gateway_method.this[each.value.resource_path].http_method
+  integration_http_method = each.value.integration_http_method #aws_api_gateway_method.this[each.value.resource_path].http_method
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  # request_templates = {
+  #   "application/json" = ""
+  #   "application/xml"  = "#set($inputRoot = $input.path('$'))\n{ }"
+  # }
+
+  request_templates = {
+    "application/json" = <<EOF
+##  See https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
+#set($allParams = $input.params())
+{
+"content": "$input.body",
+"body-json" : $input.json('$'),
+"params" : {
+#foreach($type in $allParams.keySet())
+    #set($params = $allParams.get($type))
+"$type" : {
+    #foreach($paramName in $params.keySet())
+    "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+        #if($foreach.hasNext),#end
+    #end
+}
+    #if($foreach.hasNext),#end
+#end
+},
+"stage-variables" : {
+#foreach($key in $stageVariables.keySet())
+"$key" : "$util.escapeJavaScript($stageVariables.get($key))"
+    #if($foreach.hasNext),#end
+#end
+},
+"context" : {
+    "account-id" : "$context.identity.accountId",
+    "api-id" : "$context.apiId",
+    "api-key" : "$context.identity.apiKey",
+    "authorizer-principal-id" : "$context.authorizer.principalId",
+    "caller" : "$context.identity.caller",
+    "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+    "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+    "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+    "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+    "http-method" : "$context.httpMethod",
+    "stage" : "$context.stage",
+    "source-ip" : "$context.identity.sourceIp",
+    "user" : "$context.identity.user",
+    "user-agent" : "$context.identity.userAgent",
+    "user-arn" : "$context.identity.userArn",
+    "request-id" : "$context.requestId",
+    "resource-id" : "$context.resourceId",
+    "resource-path" : "$context.resourcePath"
+    }
+}
+  EOF
+  }
 }
 
 data "aws_lambda_function" "lambda" {
